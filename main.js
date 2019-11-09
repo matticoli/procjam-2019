ab0a3de6f2020dd2bb0b70ba769484062f5cc085
Diana <dkumykova@wpi.edu>
2019-11-08 19:56:09 -0500

    idk what im doing

--- main.js
+++ main.js
@@ -5,17 +5,21 @@ var thing = Filtrr2("#img", function() {
     console.log("Test2");      
     
     var dup = this.dup().expose(80);
-    this.layer('overlay', dup).render();
+    this.layer('overlay', dup);
     //this.saturate(-70).render();
 });
 
-function help(){
+
+
+function applyFilter(){
     console.log("asdfasdfasdfas");
 
     thing.update(function(){
         this.saturate(-80).render();
     });
-    // thing.ready(function(){
-    //     this.saturate(80).render();
-    // });
+    var img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
+    img.attr('src', "./assets/_0001s_0000_flower-face-skin.png");
+    
+    img.appendTo('#doll');
+    img.css('position', 'absolute');
 }
