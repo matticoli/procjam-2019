507fdbdfc433de20794e2a783358ea30a3009f5c
Diana <dkumykova@wpi.edu>
2019-11-08 19:29:27 -0500

    button

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 console.log("Test");              
-Filtrr2("#img", function() {  
-    console.log("Test2");              
-    this.expose(30).render();
+
+//define original filter and update
+var thing = Filtrr2("#img", function() {  
+    console.log("Test2");      
+    
+    var dup = this.dup().expose(80);
+    this.layer('overlay', dup).render();
+    //this.saturate(-70).render();
 });
+
+function help(){
+    console.log("asdfasdfasdfas");
+
+    thing.update(function(){
+        this.saturate(-80).render();
+    });
+    // thing.ready(function(){
+    //     this.saturate(80).render();
+    // });
+}
