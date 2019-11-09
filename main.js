2dc9baea2cee5ab11e48a97583b61af972bc0e17
Diana <dkumykova@wpi.edu>
2019-11-09 17:12:45 -0500

    color picker and filter base

--- main.js
+++ main.js
@@ -8,8 +8,13 @@ var thing = Filtrr2("#img", function() {
     this.layer('overlay', dup);
     //this.saturate(-70).render();
 });
+var pickerColor;
+$("#picker").spectrum({
+    color: "#f00"
+    
+});
 
-
+$("#picker").spectrum("get");
 
 function applyFilter(){
     console.log("asdfasdfasdfas");
@@ -19,7 +24,16 @@ function applyFilter(){
     });
     var img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
     img.attr('src', "./assets/_0001s_0000_flower-face-skin.png");
-    
-    img.appendTo('#doll');
     img.css('position', 'absolute');
+    img.appendTo('#doll');//console.log(color.toHexString());
+    console.log($("#picker").spectrum("get")._r);
+    console.log($("#picker").spectrum("get")._g);
+    console.log($("#picker").spectrum("get")._b);
+    console.log($("#picker").spectrum("get")._a);
+
+    console.log($("#picker").spectrum("option", "color")); //returns original value only
+    console.log(pickerColor);
+    //console.log(document.getElementById('#picker').color.toHexString());
+    
 }
+
